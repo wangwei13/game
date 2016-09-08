b6141d93e7f181dc1e26b790e951d52d4feb567a
wangwei13 <895738144@qq.com>
2016-09-08 19:21:58 +0800

    提交

--- js/index.js
+++ js/index.js
@@ -2,13 +2,72 @@ var chess = document.getElementById('chess');
 var ctx = chess.getContext('2d');
 var logo = new Image();
 var me = true;
+var over = false;
 var chessBorad = [];
+
+
+//赢法数组
+var wins = [];
+
+//赢法统计数组
+var myWin = [];
+var computerWin = [];
 for(var i = 0; i < 15; i++){
 	chessBorad[i] = [];
 	for(var j = 0; j < 15; j++){
 		chessBorad[i][j] = 0;
 	}
 }
+//初始化三维数组，前两维是棋盘
+for(var i = 0; i < 15;i++){
+	wins[i]=[];
+	for(var j = 0; j < 15; j++){
+		wins[i][j]=[];
+	}
+}
+//所有横向赢法
+var count = 0;
+for(var i = 0; i < 15; i++){
+	for(var j = 0; j < 11; j++){
+		for(var k = 0; k < 5; k++){
+			wins[i][j + k][count] = true; 
+		}
+		count++;
+	}
+}
+//所有竖向赢法
+for(var i = 0; i < 15; i++){
+	for(var j = 0; j < 11; j++){
+		for(var k = 0; k < 5; k++){
+			wins[j + k][i][count] = true; 
+		}
+		count++;
+	}
+}
+//所有斜向赢法
+for(var i = 0; i < 11; i++){
+	for(var j = 0; j < 11; j++){
+		for(var k = 0; k < 5; k++){
+			wins[i + k][j + k][count] = true; 
+		}
+		count++;
+	}
+}
+//所有反斜线赢法
+for(var i = 0; i < 11; i++){
+	for(var j = 14; j > 3; j--){
+		for(var k = 0; k < 5; k++){
+			wins[i + k][j - k][count] = true; 
+		}
+		count++;
+	}
+}
+
+for(var i = 0; i < count; i++){
+	myWin[i]=0;
+	computerWin[i]=0;
+}
+console.log(count);
 logo.src = "image/mutou.jpg";
 ctx.strokeStyle = "#BFBFBF";
 logo.onload = function(){
@@ -42,20 +101,111 @@ var oneStep = function(i, j, me){
 	ctx.fill();
 }
 chess.onclick = function(e){
+	if(over){
+		return;
+	}
+	if (!me) {
+		return;
+	};
 	var x = e.offsetX;
 	var y = e.offsetY;
 	var i = Math.floor(x/30);
 	var j = Math.floor(y/30);
 	if(chessBorad[i][j] == 0){
 		oneStep(i,j,me);
-		if(me == true){
-			chessBorad[i][j] = 1;
+		chessBorad[i][j] = 1;
+		for(var k = 0; k < count; k++){
+			if(wins[i][j][k]){
+				myWin[k]++;
+				computerWin[k] = 6;
+				if(myWin[k] == 5){
+					window.alert("你赢了");
+					over = true;
+				}
+			}
 		}
-		else{
-			chessBorad[i][j] = 2;
+		if(!over){
+			me = !me;
+			computerAI();
 		}
-		me = !me;
-
 	}
 	
+}
+var computerAI = function(){
+	var myScore = [];
+	var computerScore = [];
+	var max = 0;
+	var u = 0,v =0;
+	for(var i = 0; i < 15; i++){
+		myScore[i] = [];
+		computerScore[i] = [];
+		for(var j = 0; j < 15; j++){
+			myScore[i][j] = 0;
+			computerScore[i][j] = 0;
+		}
+	}
+	for(var i = 0; i < 15; i++){
+		for(var j = 0; j < 15; j++){
+			if(chessBorad[i][j] == 0){
+				for(var k = 0; k < count; k++){
+					if(wins[i][j][k]){
+						if(myWin[k] == 1){
+							myScore[i][j] += 200;
+						}else if(myWin[k] == 2){
+							myScore[i][j] += 400;
+						}else if(myWin[k] == 3){
+							myScore[i][j] +=2000;
+						}else if(myWin[k] == 4){
+							myScore[i][j] += 10000;
+						}
+						if(computerWin[k] == 1){
+							computerScore[i][j] += 220;
+						}else if(computerWin[k] == 2){
+							computerScore[i][j] += 420;
+						}else if(computerWin[k] == 3){
+							computerScore[i][j] +=2100;
+						}else if(computerWin[k] == 4){
+							computerScore[i][j] += 20000;
+						}
+					}
+
+				}
+				if(myScore[i][j] > max){
+					max = myScore[i][j];
+					u = i;
+					v = j;
+				}else if(myScore[i][j] ==max){
+					if(computerScore[i][j] > computerScore[u][v]){
+						u = i;
+						v = j;
+					}
+				}
+				if(computerScore[i][j] > max){
+					max = computerScore[i][j];
+					u = i;
+					v = j;
+				}else if(computerScore[i][j] ==max){
+					if(myScore[i][j] > myScore[u][v]){
+						u = i;
+						v = j;
+					}
+				}
+			}
+		}
+	}
+	oneStep(u,v,false);
+	chessBorad[u][v] = 2;
+	for(var k = 0; k < count; k++){
+			if(wins[u][v][k]){
+				computerWin[k]++;
+				myWin[k] = 6;
+				if(computerWin[k] == 5){
+					window.alert("计算机赢了");
+					over = true;
+				}
+			}
+		}
+		if(!over){
+			me = !me;
+		}
 }
